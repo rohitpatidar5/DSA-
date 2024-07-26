dae1a8a9a82b6add419da1f05cc3433f4bfb793f
Rohit Patidar <rohitpatidar583@gmail.com>
2024-07-26 21:19:28 +0530

    fabonaci series

---
+++ recursion/fabonaci.cpp
@@ -0,0 +1,41 @@
+#include<iostream>
+using namespace std;
+
+int main()
+{
+
+//  method 1
+//     int a =0;
+//     int b =1;
+//     int n = 10;
+
+//    cout<< a <<","<< b << ",";
+//     for(int i =2; i<n;i++)
+//     {
+//         int c = a + b;
+//         cout<< c <<",";
+//         a = b;
+//         b= c;
+//     }
+
+
+
+//method 2
+// int arr[100];
+
+//  arr[0]= 0;
+//  arr[1]= 1;
+
+//  for(int i =2; i< 20; i++)
+//  {
+//     arr[i] = arr[i-1]+ arr[i-2];
+//  }
+
+// for(int i =0; i< 20; i++)
+// {
+//     cout<< arr[i]<<",";
+// }
+
+
+    return 0;
+}
